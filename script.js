cb37eec1c692384aee472d0d6e810980202c0662
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-08-23 12:35:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-class Person{
-	constructor(name,age){
-		this.name=name;
-		this.age=age;
-	}
-    greet(){
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+// Person class definition
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
     }
 }
-class Employee extends Person{
-	constructor(name,age,jobTitle){
-		super(name, age);
-		this.jobTitle=jobTitle;
-	}
-    jobGreet() 
-        {
-			console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
-		}
+
+// Employee class definition, extending Person
+class Employee extends Person {
+    constructor(name, age, jobTitle) {
+        super(name, age); // Call the parent class constructor
+        this.jobTitle = jobTitle;
+    }
+
+    jobGreet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    }
 }
 
-const person=new Person("Alice",25);
-person.greet();
-
-const employee=new Employee("Bob",30,"Manager");
-employee.jobGreet();
+// Attach classes to the window object
+window.Person = Person;
+window.Employee = Employee;
